2e637ac04ff00471e71a804d1ec4e814f059c5ec
Cody Murdock <warhammer4563@gmail.com>
2020-02-26 14:56:22 -0600

    finished chapter 6 exercises

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,11 +3,12 @@ import React from "react";
 import Home from "./home/Home";
 import AnimalCard from "./animal/AnimalCard";
 //only include these once they are built - previous practice exercise
-import AnimalList from "./animal/AnimalList"
-import EmployeeList from "./employee/EmployeeList"
-import OwnerList from "./owner/OwnerList"
-import LocationList from "./location/LocationList"
-
+import AnimalList from "./animal/AnimalList";
+import EmployeeList from "./employee/EmployeeList";
+import OwnerList from "./owner/OwnerList";
+import LocationList from "./location/LocationList";
+import AnimalDetail from "./animal/AnimalDetail"
+import LocationDetail from "./location/LocationDetail"
 
 const ApplicationViews = () => {
   return (
@@ -20,11 +21,30 @@ const ApplicationViews = () => {
         }}
       />
       <Route
+        exact
         path="/animals"
         render={props => {
           return <AnimalList />;
         }}
       />
+      <Route
+        path="/animals/:animalId(\d+)"
+        render={props => {
+          // Pass the animalId to the AnimalDetailComponent
+          return (
+            <AnimalDetail animalId={parseInt(props.match.params.animalId)} />
+          );
+        }}
+      />
+
+      {/*
+  This is a new route to handle a URL with the following pattern:
+  http://localhost:3000/animals/1
+
+  It will not handle the following URL because the `(\d+)`
+  matches only numbers after the final slash in the URL
+  http://localhost:3000/animals/jack
+*/}
       <Route
         path="/employee"
         render={props => {
@@ -34,15 +54,19 @@ const ApplicationViews = () => {
       <Route
         path="/owner"
         render={props => {
-          return <OwnerList/>;
+          return <OwnerList />;
         }}
       />
       <Route
-        path="/location"
+        exact path="/location"
         render={props => {
           return <LocationList />;
         }}
       />
+      <Route path="/locations/:locationId(\d+)" render={(props) => {
+  // Pass the locationId to the LocationDetailComponent
+  return <LocationDetail locationId={parseInt(props.match.params.locationId)}/>
+}} />
     </React.Fragment>
   );
 };
--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Animal.css";
+import { Link } from "react-router-dom"
 
 const AnimalCard = props => {
   return (
@@ -12,7 +13,15 @@ const AnimalCard = props => {
           Name: <span className="content-petname">{props.animal.name}</span>
         </h3>
         <p>Breed: {props.animal.breed}</p>
-        <button type="button" onClick={() => props.deleteAnimal(props.animal.id)}>Discharge</button>
+        <button
+          type="button"
+          onClick={() => props.deleteAnimal(props.animal.id)}
+        >
+          Discharge
+        </button>
+        <Link to={`/animals/${props.animal.id}`}>
+          <button>Details</button>
+        </Link>
       </div>
     </div>
   );
